9956d5f70ef50fc7e8ff2ec43bafc20a506b8db9
LeCleric <13441191+LeCleric@users.noreply.github.com>
2018-02-25 15:41:24 -0500

    Created Mocked version of the shouldAbleFindValidBudget() test.

--- src/test/java/io/budgetapp/resource/MockBudgetResourceIT.java
+++ src/test/java/io/budgetapp/resource/MockBudgetResourceIT.java
@@ -20,6 +20,8 @@ import io.budgetapp.model.Budget;
 import io.budgetapp.model.form.budget.AddBudgetForm;
 import io.budgetapp.model.form.budget.UpdateBudgetForm;
 
+import static org.hamcrest.CoreMatchers.is;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(ResourceIT.class)
 public class MockBudgetResourceIT {
@@ -89,4 +91,45 @@ public class MockBudgetResourceIT {
 		assertEquals(100, updatedBudgetEntity.getProjected(), 0.000);
 	
 	}	
+	/**
+	 * Created by @LeCleric 
+	 * @throws URISyntaxException 
+	 */
+	@Test
+	public void shouldAbleFindValidBudget() throws URISyntaxException {
+		
+		//Values needed for testing are initialized
+		String name = "Name";
+		long categoryId = 10;
+		String path = "/api/budgets/10";
+		URI testURI = new URI("http://localhost:9999/api/budgets/10");
+		
+		//Mock static methods of ResourceIT
+		PowerMockito.mockStatic(ResourceIT.class);
+		//Mock instance of ResourceIT
+		ResourceIT mockedResourceIT = mock(ResourceIT.class);
+		
+		//Create addBudgetForm
+        AddBudgetForm addBudget = new AddBudgetForm();
+		addBudget.setName(name);
+		addBudget.setCategoryId(categoryId);
+
+		//Create mocked HTTP responses
+		Response mockedPostResponse = mock(Response.class);
+		Response mockedGetResponse = mock(Response.class);
+		
+		//Stub POST, get & getLocation calls
+		when(ResourceIT.post(path, addBudget)).thenReturn(mockedPostResponse);
+		when(mockedPostResponse.getLocation()).thenReturn(testURI);
+		when(mockedResourceIT.get(path)).thenReturn(mockedGetResponse);
+		
+        // then create a newResponse Response calling the previously stubbed methods
+        Response newResponse = mockedResourceIT.get(ResourceIT.post(path, addBudget).getLocation().getPath());
+        
+        //Stub the getStatus call (having the test work with mockedGetResponse proves that newResponse contains mockedGetResponse
+        when(mockedGetResponse.getStatus()).thenReturn(200);
+        
+        //this is the equivalent of the assertOk method in ResourceIT
+        assertThat(newResponse.getStatus(), is(200));
+	}
 }
