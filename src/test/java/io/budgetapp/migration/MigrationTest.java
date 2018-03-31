f5ad35be66e6bad5712a92cbc6a4a2ceada7ef00
FrankSauve <franksauve@outlook.com>
2018-03-31 14:51:34 -0400

    Added forklifts for budget and budget_types

--- src/test/java/io/budgetapp/migration/MigrationTest.java
+++ src/test/java/io/budgetapp/migration/MigrationTest.java
@@ -1,6 +1,7 @@
 package io.budgetapp.migration;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -25,14 +26,14 @@ public class MigrationTest{
 	String username2 = "root";
 	String password2 = "root";
 
-	public void forklift() {
+	/**
+	 * Migrates the user table
+	 */
+	public void forkliftUsers() {
 
-		LOGGER.info("**************Forklift Active***************");
-		LOGGER.info("**************Tranferring Data***************");
 		try {
 			Connection conPostgres =  DriverManager.getConnection(host1,username1,password1);
 			Connection conMySQL = DriverManager.getConnection(host2,username2,password2);
-
 
 			Statement stmtPostgres = conPostgres.createStatement( );
 			ResultSet result = stmtPostgres.executeQuery("SELECT * FROM users");
@@ -56,8 +57,8 @@ public class MigrationTest{
 
 
 				// Copying data into new storage (MySQL)
-				String query = " insert into users (username, password, name, created_at, currency)"
-						+ " values (?, ?, ?, ?, ?)";
+				String query = " INSERT INTO users (username, password, name, created_at, currency)"
+						+ " VALUES (?, ?, ?, ?, ?)";
 				PreparedStatement preparedStmt = conMySQL.prepareStatement(query);
 				preparedStmt.setString(1, username);
 				preparedStmt.setString(2, password);
@@ -73,13 +74,129 @@ public class MigrationTest{
 					LOGGER.error("username " + username + " already exists");
 				}
 			}
-
 			conMySQL.close();
 			conPostgres.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
-
 		}
+	}
+	
+	/**
+	 * Migrate the budget_types data
+	 */
+	public void forkliftBudgetTypes() {
+		try {
+			Connection conPostgres =  DriverManager.getConnection(host1,username1,password1);
+			Connection conMySQL = DriverManager.getConnection(host2,username2,password2);
+
+			Statement stmtPostgres = conPostgres.createStatement( );
+			ResultSet result = stmtPostgres.executeQuery("SELECT * FROM budget_types");
+
+			while(result.next()) {
+
+				// Getting values from the old database (Postgres)
+				int id = result.getInt("id");
+				LOGGER.debug("id: " + id);
+
+				Timestamp timeStamp = result.getTimestamp("created_at");
+				LOGGER.debug("created_at" + timeStamp );
+				
+				// Copying data into new storage (MySQL)
+				String query = " INSERT INTO budget_types (created_at) VALUES (?)";
+				PreparedStatement preparedStmt = conMySQL.prepareStatement(query);
+				preparedStmt.setTimestamp(1, timeStamp);
+				
+				try {
+					preparedStmt.execute();
+				}
+				catch (SQLIntegrityConstraintViolationException  e) {
+					LOGGER.error("budget_types table migration failed");
+					e.printStackTrace();
+				}
+			}
+			conMySQL.close();
+			conPostgres.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Migrate the Budgets table
+	 */
+	public void forkliftBudgets() {
+		try {
+			Connection conPostgres =  DriverManager.getConnection(host1,username1,password1);
+			Connection conMySQL = DriverManager.getConnection(host2,username2,password2);
+
+			Statement stmtPostgres = conPostgres.createStatement( );
+			ResultSet result = stmtPostgres.executeQuery("SELECT * FROM budgets");
+
+			while(result.next()) {
+
+				// Getting values from the old database (Postgres)
+				int id = result.getInt("id");
+				LOGGER.debug("id: " + id);
+
+				String name = result.getString("name");
+				LOGGER.debug("name: " + name);
+				double projected = result.getDouble("projected");
+				LOGGER.debug("projected: " + projected);
+				double actual = result.getDouble("actual");
+				LOGGER.debug("actual: " + actual);
+				Date periodOn= result.getDate("period_on");
+				LOGGER.debug("period_on" + periodOn );
+				Timestamp timeStamp = result.getTimestamp("created_at");
+				LOGGER.debug("created_at" + timeStamp );
+				int userId = result.getInt("user_id");
+				LOGGER.debug("user_id" + userId);
+				int categoryId = result.getInt("category_id");
+				LOGGER.debug("category_id" + categoryId);
+				int typeId = result.getInt("type_id");
+				LOGGER.debug("type_id" + typeId);
+				
+				//Disable foreign key checks
+				Statement disableFKChecks = conMySQL.createStatement();
+				disableFKChecks.executeQuery("SET FOREIGN_KEY_CHECKS=0");
+
+				// Copying data into new storage (MySQL)
+				String query = " INSERT INTO budgets (name, projected, actual, period_on, created_at, user_id, category_id, type_id)"
+						+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+				PreparedStatement preparedStmt = conMySQL.prepareStatement(query);
+				preparedStmt.setString(1, name);
+				preparedStmt.setDouble(2, projected);
+				preparedStmt.setDouble(3, actual);
+				preparedStmt.setDate(4, periodOn);
+				preparedStmt.setTimestamp(5, timeStamp);
+				preparedStmt.setInt(6, userId);
+				preparedStmt.setInt(7, categoryId);
+				preparedStmt.setInt(8, typeId);
+
+				try {
+					preparedStmt.execute();
+				}
+				catch (SQLIntegrityConstraintViolationException  e) {
+					LOGGER.error("budgets table migration failed");
+					e.printStackTrace();
+				}
+				
+				//Enable foreign key checks
+				Statement enableFKChecks = conMySQL.createStatement();
+				enableFKChecks.executeQuery("SET FOREIGN_KEY_CHECKS=1");
+			}
+			conMySQL.close();
+			conPostgres.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void forklift() {
+		LOGGER.info("**************Forklift Active***************");
+		LOGGER.info("**************Tranferring Data***************");
+		forkliftUsers();
+		forkliftBudgetTypes();
+		forkliftBudgets();
 		LOGGER.info("**************Forklift Deactive***************");
 		LOGGER.info("**************Transfer Stopped****************");
 	}
