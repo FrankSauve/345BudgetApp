ec172e474dee84f854e04b606c8b3a689139fb62
daanish93 <daanishrehman93@gmail.com>
2018-03-31 00:24:51 -0400

    #14 Delected improper template created previously and made a test class with forklift.

---
+++ src/test/java/io/budgetapp/migration/MigrationTest.java
@@ -0,0 +1,33 @@
+package io.budgetapp.migration;
+
+import org.junit.Test;
+
+
+public class MigrationTest{
+
+	public void forklift() {
+		// Getting data from the old database (Postgres)
+
+		// Copying data into new storage (MySQL)
+		
+	}
+
+
+	@Test
+	public void migrationTest() {
+
+		// forklift the data from old storage to new 
+		forklift();
+
+		// check for inconsistencies 
+
+		// ensure inconsistencies are fixed 
+
+		//shadow writes 
+
+		// shadow reads for validation 
+
+
+	}
+
+}
